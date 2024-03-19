c5b30814e25d7e91569ed2c90ce6a40c8f96a25f
liuxi <liuxi@sinomartin.com>
2024-03-19 14:18:29 +0800

    模块加载css资源；测试多仓库

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
     <ClientOnly>
         <h1>Index</h1>
         <NuxtLink to="/about">关于</NuxtLink>
+        <br />
+        <NuxtLink to="/posts/1">文章1</NuxtLink>
+        <br />
+        <NuxtLink to="/posts/2">文章2</NuxtLink>
+        <br />
     </ClientOnly>
 </template>
 
---
+++ pages/posts/[id].vue
@@ -0,0 +1,9 @@
+<template>
+    <div>postID: {{ postID }}</div>
+</template>
+
+<script setup>
+const postID = useRoute().params.id;
+</script>
+
+<style></style>
